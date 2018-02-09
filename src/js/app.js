55fbb71ec1f86a5a8ff11c066ee2f63075b3d9c7
Fabienne Prouteau <faabke@gmail.com>
2018-02-09 15:09:27 +0000

    set Basic HTML layout and grid and basic scss styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,51 @@
-[10,20,30].forEach(num => console.log(num));
+$(() => {
+  console.log('JS Loaded');
+
+  //Global Variables
+  const $grid = $('.mainGrid');
+  const $sections = $('section');
+  const $topSection = $('.topSection');
+  const $middleSection= $('.middleSection');
+  const $bottomSection= $('.bottomSection');
+  const $middleSectionText = $('.middleSection p');
+  const $bottomSectionText = $('.bottomSection p');
+  const $marioIntro = $('.marioIntro');
+  const $goButton = $('.goButton');
+
+
+  let level = 1;
+  let nbLives = 3;
+
+  //Functions
+  //displayes 1st frame
+  function firstFrame(){
+    //make the grid disappear
+    $grid.hide();
+    //hide mario picture - but still could see it for 1 sec
+    // $marioIntro.hide();
+    //show all sections
+    $sections.show();
+
+  }
+
+  //displays second frame
+  function secondFrame(e){
+    //changes top section to Level X
+    $topSection.text(`Level ${level}`);
+    //changes middle section to Level number of life and add Mario pic
+    $middleSectionText.text(`   X   ${nbLives}`);
+    $marioIntro.show();
+    //changes bottom section to instruction
+    $bottomSectionText.html('Marioooo, try to catch as many <img src="/images/coin-small.png" alt=" coins "> as possible and avoid the <img src="/images/ennemyMushroom-small.png" alt=" ennemies ">');
+    $goButton.show();
+  }
+
+  //Link functions with DOM elements
+  $middleSection.on('click',secondFrame);
+  //$goButton.on('click',gameStart);
+
+  //Call function to start game
+  firstFrame();
+
+
+});
