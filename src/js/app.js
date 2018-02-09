45c4f77023c995d6f2a42e9b37af1eae778e27d9
Fabienne Prouteau <faabke@gmail.com>
2018-02-09 16:44:46 +0000

    started animation of the coins- still figuring out how to make transition smoother

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ $(() => {
 
   //Global Variables
   const $grid = $('.mainGrid');
+  const $squares = $('.square');
   const $sections = $('section');
   const $topSection = $('.topSection');
   const $middleSection= $('.middleSection');
@@ -11,13 +12,30 @@ $(() => {
   const $bottomSectionText = $('.bottomSection p');
   const $marioIntro = $('.marioIntro');
   const $goButton = $('.goButton');
-
-
+  const $coin = $('.coin');
   let level = 1;
   let nbLives = 3;
+  //nb of squares = 152; 19 columns * 8 rows
+
+
+  //TO DO
+  //make site responsive for ipad - grid should not collapse
+
+  //class definitions
+  class Ennemy{
+    constructor(name,image){
+      this.name = name;
+      this.image = image;
+    }
+
+    // animates{
+    //
+    // }
+  };
+
 
   //Functions
-  //displayes 1st frame
+  //displays 1st frame
   function firstFrame(){
     //make the grid disappear
     $grid.hide();
@@ -40,12 +58,45 @@ $(() => {
     $goButton.show();
   }
 
+  //Frame to start game
+  function gameStart(){
+    //change background picture
+    // ------------> still to do
+    //Show the grid
+    $grid.show();
+    //hide the $sections
+    $sections.hide();
+  }
+
+  function animateCoin(){
+    setTimeout(() => {
+      $($squares[0]).html('');
+    }, 200);
+    setTimeout(() => {
+      $($squares[18]).html('<img class="coin" src="/images/coin.png" alt="coin">');
+    }, 200);
+    setTimeout(() => {
+      $($squares[18]).html('');
+    }, 300);
+    setTimeout(() => {
+      $($squares[18*2]).html('<img class="coin" src="/images/coin.png" alt="coin">');
+    }, 300);
+    setTimeout(() => {
+      $($squares[18*2]).html('');
+    }, 400);
+    setTimeout(() => {
+      $($squares[18*3]).html('<img class="coin" src="/images/coin.png" alt="coin">');
+    }, 400);
+  }
+
   //Link functions with DOM elements
   $middleSection.on('click',secondFrame);
-  //$goButton.on('click',gameStart);
+  $goButton.on('click',gameStart);
 
-  //Call function to start game
-  firstFrame();
+  //Call function to initialise first screen
+  //firstFrame();
+  gameStart();
+  animateCoin();
 
 
 });
