724f0a41656088459b5cae0dde9ea75bc36162fa
Fabienne Prouteau <faabke@gmail.com>
2018-02-10 10:32:22 +0000

    created level 1 animations partially and added game logic with number of coins increasing when mario catches one

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,7 @@ $(() => {
   const $bottomSection= $('.bottomSection');
   const $middleSectionText = $('.middleSection p');
   const $bottomSectionText = $('.bottomSection p');
+  const $coinsSpan = $('.coinsSpan');
   const $marioIntro = $('.marioIntro');
   const $goButton = $('.goButton');
   const $coin = $('.coin');
@@ -21,7 +22,11 @@ $(() => {
   let totalNbSquares = nbColumns * nbRows;
   let coin = '<img class="coin" src="/images/coin.png" alt="coin">';
   let ennemyMushroom = '<img class="ennemy" src="/images/ennemyMushroom.png" alt="ennemy">';
-
+  let mario = '<img class="mario" src="/images/littleMario.png" alt="mario">';
+  let marioPos = 136;
+  let leftPos = 133; // to calculate depending on grid size
+  let rightPos = 152; // to calculate depending on grid size
+  let nbCoins = 0;
 
   //TO DO
   //make site responsive for ipad
@@ -54,6 +59,7 @@ $(() => {
     $grid.show();
     //hide the $sections
     $sections.hide();
+    level1();
   }
 
 
@@ -67,60 +73,104 @@ $(() => {
   // incrTimeOut: Timeout beteen each step of animation, the highest the number the slower the animation
   // src: which ennemy, which gives image to use
 
-  function Level1(){
-    //Duration 60 second so 6000 ms
+  function level1(){
+    //set speed for the level - Duration 60 second so 6000 ms
+    const avgIncrTimeOut = 400;
+    const rapidIncrTimeOut = 200;
     //change background picture
     // ------------> still to do
-    let avgIncrTimeOut = 400;
-    let rapidIncrTimeOut = 200;
-    animateElement(0, 200, avgIncrTimeOut, coin);
-    animateElement(6, 1000, avgIncrTimeOut, coin);
-    animateElement(5, 2000, rapidIncrTimeOut, ennemyMushroom);
-    animateElement(9, 3000, avgIncrTimeOut, coin);
-    animateElement(13, 4000, avgIncrTimeOut, coin);
-    animateElement(6, 5000, avgIncrTimeOut, coin);
-    animateElement(9, 6000, rapidIncrTimeOut, ennemyMushroom);
-    animateElement(15, 7000, avgIncrTimeOut, coin);
-    animateElement(11, 8000, avgIncrTimeOut, coin);
-    animateElement(3, 9000, avgIncrTimeOut, coin);
-    animateElement(7, 9000, rapidIncrTimeOut, ennemyMushroom);
-    animateElement(17, 10000, avgIncrTimeOut, coin);
-    animateElement(12, 12000, avgIncrTimeOut, coin);
-    animateElement(15, 12000, rapidIncrTimeOut, ennemyMushroom);
-    animateElement(3, 13000, avgIncrTimeOut, coin);
-    animateElement(19, 14000, avgIncrTimeOut, coin);
-    animateElement(10, 15000, avgIncrTimeOut, coin);
-    animateElement(18, 15000, avgIncrTimeOut, coin);
-    animateElement(15, 12000, rapidIncrTimeOut, ennemyMushroom);
-    animateElement(4, 16000, avgIncrTimeOut, coin);
-    animateElement(8, 17000, avgIncrTimeOut, coin);
+    //set Mario on 3rd square on the left
+    $($squares[marioPos]).html(mario);
+    $($squares[marioPos]).toggleClass('mario');
+    //Animate coins, ennemies and bonus
+    animateElement(0, 200, avgIncrTimeOut, coin, 'coin');
+    animateElement(6, 1000, avgIncrTimeOut, coin, 'coin');
+    animateElement(5, 2000, rapidIncrTimeOut, ennemyMushroom, 'ennemy');
+    animateElement(9, 3000, avgIncrTimeOut, coin, 'coin');
+    animateElement(13, 4000, avgIncrTimeOut, coin, 'coin');
+    animateElement(6, 5000, avgIncrTimeOut, coin, 'coin');
+    animateElement(9, 6000, rapidIncrTimeOut, ennemyMushroom, 'ennemy');
+    animateElement(15, 7000, avgIncrTimeOut, coin, 'coin');
+    animateElement(11, 8000, avgIncrTimeOut, coin, 'coin');
+    animateElement(3, 9000, avgIncrTimeOut, coin, 'coin');
+    animateElement(7, 9000, rapidIncrTimeOut, ennemyMushroom, 'ennemy');
+    animateElement(17, 10000, avgIncrTimeOut, coin, 'coin');
+    animateElement(12, 12000, avgIncrTimeOut, coin, 'coin');
+    animateElement(15, 12000, rapidIncrTimeOut, ennemyMushroom, 'ennemy');
+    animateElement(3, 13000, avgIncrTimeOut, coin, 'coin');
+    animateElement(19, 14000, avgIncrTimeOut, coin, 'coin');
+    animateElement(10, 15000, avgIncrTimeOut, coin, 'coin');
+    animateElement(18, 15000, avgIncrTimeOut, coin, 'coin');
+    animateElement(15, 12000, rapidIncrTimeOut, ennemyMushroom, 'ennemy');
+    animateElement(4, 16000, avgIncrTimeOut, coin, 'coin');
+    animateElement(8, 17000, avgIncrTimeOut, coin, 'coin');
     //to continue until 60 seconds
     //animateElement(6, 60000, averageSpeed, coin);
   }
 
-  function animateElement(column, InitialTimeOut, incrTimeOut, src){
+  function animateElement(column, InitialTimeOut, incrTimeOut, src, type){
     //initialise timeout
     let timeOutIncrement = InitialTimeOut;
     //loop to animate coin in the column
     for (let i = 0 + column; i < totalNbSquares; i = i + nbColumns){
+      //animate the element by making it appear and disappear from 1 square to the other
+      //if Mario is in a specific square, no animation
       setTimeout(() => {
-        $($squares[i]).html(src);
+        //set class 'type' on the div
+        $($squares[i]).toggleClass(type);
+        //check for a win
+        if ($($squares[i]).hasClass('mario') === true){
+          //increment nbCoins and update number of coins on screen
+          nbCoins++;
+          $coinsSpan.text(parseInt($coinsSpan.text()) + 1);
+        } else { //do not show element image if mario is in the div
+          $($squares[i]).html(src);
+        }
       }, timeOutIncrement);
       timeOutIncrement = timeOutIncrement + incrTimeOut;
       setTimeout(() => {
-        $($squares[i]).html('');
+        if ($($squares[i]).hasClass('mario') !== true){
+          //do not show element image if mario is in the div
+          $($squares[i]).html('');
+        }
+        //in any case toggle class 'type' on the div
+        $($squares[i]).toggleClass(type);
       }, timeOutIncrement);
     }
   }
 
-
-  //Link functions with DOM elements
+  //LINK FUNCTIONS WITH DOM ELEMENTS
   $middleSection.on('click',secondFrame);
   $goButton.on('click',gameStart);
 
+  //Mario going left and right event
+  $(document).on('keydown', function(e) {
+    if(e.which === 37){ //left arrow
+      let newPos = Math.max(marioPos - 1, leftPos); // to avoid going out of screen
+      //remove Mario from initial position
+      $($squares[marioPos]).html('');
+      $($squares[marioPos]).toggleClass('mario'); //toggle mario class on div
+      //set MArio in new position and toggle mario class
+      $($squares[newPos]).html(mario);
+      $($squares[newPos]).toggleClass('mario');
+      marioPos = newPos;
+    }
+    else if (e.which === 39){ //right arrow
+      let newPos = Math.min(marioPos + 1, rightPos); // to avoid going out of screen
+      //remove Mario from initial position
+      $($squares[marioPos]).html('');
+      $($squares[marioPos]).toggleClass('mario'); //toggle mario class on div
+      //set Mario in new position and toggle mario class
+      $($squares[newPos]).html(mario);
+      $($squares[newPos]).toggleClass('mario');
+      marioPos = newPos;
+    }
+  });
+
+
   //Call function to initialise first screen
-  //firstFrame();
-  gameStart();
-  Level1();
+  firstFrame();
+  //gameStart();
+  //Level1();
 
 });
